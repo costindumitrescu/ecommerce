307f79c924cb0729dbdf5f1bd308770161df2618
dumitrescu <dumitrescucostin20@gmail.com>
2021-02-27 16:30:44 -0500

    finished the models

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import products from './data/products.js'
+
+dotenv.config()
+
+connectDB()
+
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('API is running...')
+  res.send('API is running better ')
 })
 
 app.get('/api/products', (req, res) => {
@@ -15,4 +23,6 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Sever running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
